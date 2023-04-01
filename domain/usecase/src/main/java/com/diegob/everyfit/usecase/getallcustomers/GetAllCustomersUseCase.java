04fab0271957fc550f00e60c0ed4d9901e2bcb85
zensei <diegofer_1110@hotmail.com>
2023-03-31 19:23:35 -0500

    Get all customers done

---
+++ domain/usecase/src/main/java/com/diegob/everyfit/usecase/getallcustomers/GetAllCustomersUseCase.java
@@ -0,0 +1,19 @@
+package com.diegob.everyfit.usecase.getallcustomers;
+
+import com.diegob.everyfit.model.clothingitem.ClothingItem;
+import com.diegob.everyfit.model.customer.Customer;
+import com.diegob.everyfit.model.customer.gateways.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Supplier;
+
+@RequiredArgsConstructor
+public class GetAllCustomersUseCase implements Supplier<Flux<Customer>> {
+
+    private final CustomerRepository customerRepository;
+    @Override
+    public Flux<Customer> get() {
+        return customerRepository.getAllCustomers();
+    }
+}
