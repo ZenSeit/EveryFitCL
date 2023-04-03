e4823254ebfeda70a2ca6ef8e70692dc2c332e3e
zensei <diegofer_1110@hotmail.com>
2023-04-03 10:33:00 -0500

    get customer by id use case done

---
+++ domain/usecase/src/main/java/com/diegob/everyfit/usecase/getcustomerbyid/GetCustomerByIdUseCase.java
@@ -0,0 +1,23 @@
+package com.diegob.everyfit.usecase.getcustomerbyid;
+
+import com.diegob.everyfit.model.clothingitem.ClothingItem;
+import com.diegob.everyfit.model.clothingitem.gateways.ClothingItemRepository;
+import com.diegob.everyfit.model.customer.Customer;
+import com.diegob.everyfit.model.customer.gateways.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class GetCustomerByIdUseCase implements Function<String, Mono<Customer>> {
+
+    private final CustomerRepository customerRepository;
+
+
+    @Override
+    public Mono<Customer> apply(String CustomerId) {
+        return customerRepository.getCustomerById(CustomerId);
+    }
+}
---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/com/diegob/everyfit/mongo/data/OrderData.java
@@ -0,0 +1,25 @@
+package com.diegob.everyfit.mongo.data;
+
+import com.diegob.everyfit.model.order.OrderState;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Document(collection = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderData {
+
+    @Id
+    private String id= UUID.randomUUID().toString().substring(0,10);
+    private String customer;
+    private List<String> products;
+    private OrderState state;
+
+}
